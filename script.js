b6120fbf1c1c504a3305a9d6eb586c7268380d24
Stilla17 <89776710+Stilla17@users.noreply.github.com>
2024-10-09 10:53:07 +0500

    helloworld
    hgsdhfligasfuy8ewgfh

---
+++ script.js
@@ -0,0 +1,52 @@
+//  + - * / %
+
+// let s = 240
+// let t = 60
+// let v = s / t
+// console.log(v);
+
+// let num1 = 10
+// let num2 = 20
+
+// console.log(num1 + num2 + " qo'shish amali");
+// console.log(num1 - num2 + " ayirish");
+// console.log(num1 * num2 + " kopaytrsih");
+// console.log(num1 / num2 + " bolish");
+// console.log(10 % 9);
+
+// floor(), round(), ceil(), random(), max(), min()
+
+// let number = Math.floor(4.1) //4
+// let number = Math.ceil(4.9) //4 +1
+// let number = Math.round(4.4)
+// let number = Math.random()
+// let res = Math.ceil(Math.random() * 10)
+
+// let result = Math.max(132, 43, 45, 132, 353, 34, 312, 45)
+// let result1 = Math.min(132, 43, 45, 132, 353, 34, 312, 45)
+
+// console.log(result1);
+
+// let random = Math.ceil(Math.random() * 10)
+// alert(random)
+// let myNumber = prompt("son kiritining")
+// console.log(random * myNumber);
+
+// let asdd = +prompt("son kiriting")
+// let asdd = Number(prompt("son kiriting"))
+// let asdd = parseInt(prompt("son kiriting"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
